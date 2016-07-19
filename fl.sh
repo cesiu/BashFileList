5924e462107b3d8ecc00d8a7574b944ab59e0ebf
cesiu <christopher@get2thecrossing.com>
2016-07-19 13:23:47 -0700

    Added add function.

--- fl.sh
+++ fl.sh
@@ -3,7 +3,9 @@
 # Represents a linked list using files as nodes.
 # author: Christopher (cesiu)
 
-# Gets the nth thing in a linked list of files.
+# Gets the nth element in a linked list of files.
+# $1 - the current node
+# $2 - n
 function get {
    current=$(tail -n 1 $1)
    rest=$(head -n 1 $1)
@@ -16,7 +18,18 @@ function get {
    fi
 }
 
+# Adds an element to the head of the list.
+# $1 - the element
+function add {
+   oldHead=$(head -n 1 head)
+   let size=$(tail -n 1 head)+1
+
+   printf "%s\n%s" $oldHead $1 > node$size
+   printf "%s\n%s" node$size $size > head
+}
+
 # Prints out an entire linked list of files (non-nested)
+# $1 - the current node
 function printList {
    current=$(tail -n 1 $1)
    rest=$(head -n 1 $1)
@@ -29,5 +42,25 @@ function printList {
 
 # Example usage:
 cd demo
-printList "node1"
+
+# Print the list.
+head=$(head -n 1 head)
+echo "Printing list..."
+printList $head
+
+# Add to the list.
+echo "Adding foobar..."
+add "foobar"
+
+# Print the list.
+head=$(head -n 1 head)
+printList $head
+
+# Get an element.
+echo "Getting the element at index 1..."
+get $head 1
+
+# Reset the example.
+rm node4
+printf "node1\n3" > head
 cd ..
