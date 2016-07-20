f1e91a8475ba9da641e938016dff8f1e6bf5956d
cesiu <christopher@get2thecrossing.com>
2016-07-20 13:01:26 -0700

    Added a remove function. Add function now takes the name of the list.

--- fl.sh
+++ fl.sh
@@ -19,13 +19,27 @@ function get {
 }
 
 # Adds an element to the head of the list.
-# $1 - the element
+# $1 - the head of the list to add to
+# $2 - the element to add
 function add {
-   oldHead=$(head -n 1 head)
-   let size=$(tail -n 1 head)+1
+   oldHead=$(head -n 1 $1)
+   let size=$(tail -n 1 $1)+1 
 
-   printf "%s\n%s" $oldHead $1 > node$size
-   printf "%s\n%s" node$size $size > head
+   printf "%s\n%s" $oldHead $2 > node$size
+   printf "%s\n%s" node$size $size > $1
+}
+
+# Removes an element from the head of the list.
+# $1 - the head of the list to add to
+function remove {
+   oldHead=$(head -n 1 $1)
+   oldVal=$(tail -n 1 $oldHead)
+   let size=$(tail -n 1 $1)-1
+   newHead=$(head -n 1 $oldHead)
+   
+   printf "%s\n%s" $newHead $size > $1
+   rm $oldHead
+   echo $oldVal
 }
 
 # Prints out an entire linked list of files (non-nested)
@@ -44,23 +58,38 @@ function printList {
 cd demo
 
 # Print the list.
-head=$(head -n 1 head)
+node1=$(head -n 1 list1)
 echo "Printing list..."
-printList $head
+printList $node1
 
 # Add to the list.
-echo "Adding foobar..."
-add "foobar"
+printf "\nAdding hello...\n"
+add "list1" "hello"
 
 # Print the list.
-head=$(head -n 1 head)
-printList $head
+node1=$(head -n 1 list1)
+printList $node1
+
+# Add to the list.
+printf "\nAdding world...\n"
+add "list1" "world"
+
+# Print the list.
+node1=$(head -n 1 list1)
+printList $node1
 
 # Get an element.
-echo "Getting the element at index 1..."
-get $head 1
+printf "\nGetting the element at index 1...\n"
+get $node1 1
+
+# Remove from the list.
+printf "\nRemoved %s...\n" $(remove "list1")
+
+# Print the list.
+node1=$(head -n 1 list1)
+printList $node1
 
 # Reset the example.
 rm node4
-printf "node1\n3" > head
+printf "node1\n3" > list1
 cd ..
